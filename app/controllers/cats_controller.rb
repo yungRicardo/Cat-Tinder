88a51fd9a923813cc9ce7902129509053a095c3a
Chinchi <83244996+yungRicardo@users.noreply.github.com>
2024-02-11 00:31:29 -0800

    added some controller functions for cat and some endpoints as well

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -2,11 +2,42 @@ class CatsController < ApplicationController
    def index
       cats = Cat.all
       render json: cats
+   end
 
    def show
       cat = Cat.find(params[:id])
       render json: cat
    end
+
+
+   def create
+      cat = Cat.new(cat_params)
+      if cat.save
+         render json: cat, status: :created
+      else
+         render json: cat.errors, status: :unprocessable_entity
+      end
+   end
    
-      # Implement create, update, destroy actions here
+   def update
+      cat = Cat.find(params[:id])
+      if cat.update(cat_params)
+         render json: cat
+      else
+         render json: cat.errors, status: :unprocessable_entity
+      end
+   end
+   
+   def destroy
+      cat = Cat.find(params[:id])
+      cat.destroy
+      head :no_content
+   end
+
+   private
+   
+   def cat_params
+      params.require(:cat).permit(:name, :age, :enjoys, :image)
+   end
+   
 end
