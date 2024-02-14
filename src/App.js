95459c08899768cc63c62fb89850a6159bb87a48
Chinchi <83244996+yungRicardo@users.noreply.github.com>
2024-02-13 16:12:15 -0800

    created a couple pages to navigate to from the URL using react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import CatEdit from './pages/CatEdit';
+import CatIndex from './pages/CatIndex';
+import CatNew from './pages/CatNew';
+import CatShow from './pages/CatShow';
+import NotFound from './pages/NotFound'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/catedit" element={<CatEdit />} />
+        <Route path="/catindex" element={<CatIndex />} />
+        <Route path="/catnew" element={<CatNew/>} />
+        <Route path="/catshow" element={<CatShow />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+
+    <Footer />
+    </>
   );
 }
 
 export default App;
+
+
+
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+// src/components/Footer.js
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2023 Cat Tinder. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
