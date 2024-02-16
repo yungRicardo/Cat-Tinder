a61e6e9e3c73dae085beb5b8d9c75708ec73f46b
Chinchi <83244996+yungRicardo@users.noreply.github.com>
2024-02-15 21:50:11 -0800

    Created cat cards in for the index and show page. Also created tests for Index and Show pages

--- src/pages/CatIndex.js
+++ src/pages/CatIndex.js
@@ -1,7 +1,31 @@
-const CatIndex = () => {
+import React from "react"
+import { Link } from "react-router-dom";
+import { Card, CardBody, CardTitle, CardSubtitle } from "reactstrap"
+
+const CatIndex = ({ cats }) => {
    return (
-      <></>
-   )
+      <main className="cat-index-cards">
+      {cats?.map((cat, index) => {
+         return (
+            <Card
+               style={{ width: "14rem" }}
+               key={index}
+               data-testid="cat-card" // Add this line to include the data-testid attribute
+            >
+               <img alt={`profile of a cat named ${cat.name}`} src={cat.image} />
+               <CardBody>
+                  <CardTitle tag="h5">{cat.name}</CardTitle>
+                  <CardSubtitle className="mb-2 text-muted" tag="h6">
+                     Age: {cat.age}
+                  </CardSubtitle>
+                  <a href={`/catshow/${cat.id}`} className="nav-link">See More Details</a>
+               </CardBody>
+            </Card>
+         );
+      })}
+      </main>
+   );
 };
 
-export default CatIndex;
+
+export default CatIndex
